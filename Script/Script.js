13097ba02b9c5d2ef185730d5284c50cd1eed96f
Christian <78617698+SneakyZoul@users.noreply.github.com>
2023-01-14 14:34:59 +0100

    Done woth consola rock, paper, scissors

--- Script/Script.js
+++ Script/Script.js
@@ -1,54 +1,74 @@
-let rock = "Rock";
-let paper = " Paper";
-let sicssors = "Siscssors ";
 let num;
+let playerScore =0; 
+let aiScore =0; 
+let rounds = 5;
+
 
 
 function get_ai_choies(){
     num = Math.floor(Math.random()*3 +1);
    // console.log(num + " here is num")
-   let ansawer = (num == 1)? rock : (num == 2) ? paper : (num == 3) ? sicssors: "nothing came out"
+   let ansawer = (num == 1)? "rock" : (num == 2) ? "paper" : (num == 3) ? "scissors": "nothing came out"
    return ansawer;
 }
 
-function round_of(playerSelect,AI_Select){
 
-if(playerSelect == rock && AI_Select == rock 
-    || playerSelect == paper && AI_Select == paper 
-    || playerSelect == sicssors && AI_Select == sicssors)
-{   
+function round_of()
+{
+
+    const playerSelect = prompt().toLowerCase();
+    const AI_Select = get_ai_choies();
+    
+    if(playerSelect === AI_Select )
+    {   
     console.log("Its a tie");
-}
+    }
 
-else if(playerSelect == rock && AI_Select == paper 
-        || playerSelect == paper && AI_Select == rock)
+    else if(playerSelect == "rock")
+    {
+        if(AI_Select == "paper"){
+          console.log("Ai win against rock,it chose paper")
+          aiScore++;
+        }else{
+           console.log("you win against scissors ");
+          playerScore++
+        }
+    }
+
+    else if(playerSelect == "scissors")
+    {
+        if(AI_Select == "rock"){
+        console.log("Ai win against scissors it chose rock");
+        aiScore++;
+        }else{
+        console.log("you win against paper");
+        playerScore++;
+        }
+    }
+
+    else if(playerSelect == "paper")
+    {   
+        if(AI_Select == "scissors"){
+        console.log("AI win against paper it chose scissors")
+        aiScore ++;
+        }else{
+        console.log("you win against rock");
+        playerScore++;
+        }
+    } 
+} 
+
+
+for(let i = 0; i < rounds; i++)
 {
-console.log("Paper wins");
-console.log("rock looses");
+    round_of();    
 }
 
 
-else if(playerSelect == sicssors && AI_Select == rock 
-        || playerSelect == rock && AI_Select == sicssors)
-{
-    console.log("Rock wins");
-    console.log("Sicssors looses");
-}
-
-else if(playerSelect == paper && AI_Select == sicssors 
-        || playerSelect == sicssors && AI_Select == paper)
-{
-    console.log("Sicssors win");
-    console.log("Paper losses");
-
-}
-// players ansawer should be aginst the AI
-// it should be able to se the diffence beteewn cap or not(casesenetive)
-
-}
-const playerSelect = rock;
-const AI_Select = get_ai_choies();
 
 
-console.log(round_of(playerSelect,AI_Select));
+console.log(round_of());
+console.log("here is player score " + playerScore);
+console.log("Here is AI score "+aiScore);
+
 //console.log(get_ai_choies())
